8af3f370783eb825793ec2214f4b2aa1e3120278
Jakob Erpf <contact@jakoberpf.de>
2020-10-13 15:20:30 +0200

    expand arbis dataset

--- CorrAnalysis/main_arbis_01_dataset.py
+++ CorrAnalysis/main_arbis_01_dataset.py
@@ -28,11 +28,12 @@ if __name__ == '__main__':
     data_path = 'data/'
     work_path = data_path + 'ArbIS/dataset/'
     plot_path = work_path + 'plots/'
+    tex_path = work_path + 'latex/'
     work_file = 'ArbIS_2019.csv'
 
-    arbis_import = pd.read_csv(work_path + work_file, sep=';', decimal=',', parse_dates=True, date_parser=date_parser)
+    arbis_imported = pd.read_csv(work_path + work_file, sep=';', decimal=',', parse_dates=True, date_parser=date_parser)
 
-    arbis_select_relevant = arbis_import[
+    arbis_selected = arbis_imported[
         [
             # Roadwork Data
             # 'Von', 'Bis',  # Not correlate able
@@ -51,8 +52,8 @@ if __name__ == '__main__':
         ]].copy()
 
     # Manual data type conversion from str to datetime64
-    arbis_import['Von'] = pd.to_datetime(arbis_import['Von'], format='%Y-%m-%d %H:%M:%S')
-    arbis_import['Bis'] = pd.to_datetime(arbis_import['Bis'], format='%Y-%m-%d %H:%M:%S')
+    arbis_imported['Von'] = pd.to_datetime(arbis_imported['Von'], format='%Y-%m-%d %H:%M:%S')
+    arbis_imported['Bis'] = pd.to_datetime(arbis_imported['Bis'], format='%Y-%m-%d %H:%M:%S')
 
     # Manual data type conversion str to float64
     # arbis_import['VonKilometer'] = arbis_import['VonKilometer'].replace(',', '.', regex=True).astype(float)
@@ -62,11 +63,11 @@ if __name__ == '__main__':
     # arbis_import['VonKilometer'] = arbis_import['VonKilometer'].replace(',', '.', regex=True).astype(float)
 
     # Add month of roadwork
-    arbis_select_relevant['Month'] = arbis_import['Von'].dt.month_name()
+    arbis_selected['Month'] = arbis_imported['Von'].dt.month_name()
 
     # Plot histogram of roadworks over time / months
     plt.figure(figsize=(13, 6))
-    plt.hist(arbis_select_relevant['Month'], color='blue', edgecolor='black')
+    plt.hist(arbis_selected['Month'], color='blue', edgecolor='black')
     plt.title('Histogram of roadworks per month')
     plt.ylabel('Count')
     plt.xlabel('Month of 2019')
@@ -75,23 +76,61 @@ if __name__ == '__main__':
     plt.show()
 
     # Add length of roadwork fragment in kilometers
-    arbis_select_relevant['Length'] = abs((arbis_import['VonKilometer'] - arbis_import['BisKilometer']))
+    arbis_selected['Length'] = abs((arbis_imported['VonKilometer'] - arbis_imported['BisKilometer']))
     # Add duration of roadwork fragment in minutes
-    arbis_select_relevant['Duration'] = abs((arbis_import['Von'] - arbis_import['Bis'])).dt.total_seconds() / 60
+    arbis_selected['Duration'] = abs((arbis_imported['Von'] - arbis_imported['Bis'])).dt.total_seconds() / 60
+
+    sns.set(font_scale=2)
+    sns.set_context('paper')
+    plt.figure(figsize=(11, 6))
+
+    sns.boxplot(x='Strasse', y='Length', data=arbis_selected, palette='Set1')
+    if safe_plots:
+        plt.savefig(plot_path + 'arbis_dataset_box_street2length.png')
+
+    sns.boxplot(x='Strasse', y='Duration', data=arbis_selected, palette='Set1')
+    if safe_plots:
+        plt.savefig(plot_path + 'arbis_dataset_box_stree2duration.png')
+
+    sns.boxplot(x='AnzGesperrtFs', y='Length', data=arbis_selected, palette='Set1')
+    if safe_plots:
+        plt.savefig(plot_path + 'arbis_dataset_box_agfs2length.png')
+
+    sns.boxplot(x='AnzGesperrtFs', y='Duration', data=arbis_selected, palette='Set1')
+    if safe_plots:
+        plt.savefig(plot_path + 'arbis_dataset_box_agfs2duration.png')
+
+    sns.boxplot(x='Einzug', y='Length', data=arbis_selected, palette='Set1')
+    if safe_plots:
+        plt.savefig(plot_path + 'arbis_dataset_box_einzug2length.png')
+
+    sns.boxplot(x='Einzug', y='Duration', data=arbis_selected, palette='Set1')
+    if safe_plots:
+        plt.savefig(plot_path + 'arbis_dataset_box_einzug2duration.png')
+
+    sns.boxplot(x='Richtung', y='Length', data=arbis_selected, palette='Set1')
+    if safe_plots:
+        plt.savefig(plot_path + 'arbis_dataset_box_direction2length.png')
+
+    sns.boxplot(x='Richtung', y='Duration', data=arbis_selected, palette='Set1')
+    if safe_plots:
+        plt.savefig(plot_path + 'arbis_dataset_box_direction2duration.png')
 
     # Print matrix for debugging
-    print(arbis_select_relevant.dtypes)
-    print(arbis_select_relevant)
+    print(arbis_selected.dtypes)
+    print(arbis_selected)
 
+    # defines column types
     nominal_columns = ['Strasse', 'StreckeID', 'Month']
     dichotomous_columns = ['Richtung']
-    ordinal_columns = ['AnzGesperrtFs']
+    ordinal_columns = ['AnzGesperrtFs', 'Einzug']
 
-    # Plot association matrix
-    # Calculate without Theil's U -> Cramer's V is chosen
-    arbis_encoded = numerical_encoding(arbis_select_relevant, nominal_columns, drop_single_label=False)
+    # Encode non numerical columns
+    arbis_encoded = numerical_encoding(arbis_selected, nominal_columns, drop_single_label=False)
 
-    corr, sign, columns, nominal_columns, dichotomous_columns, ordinal_columns, inf_nan, single_value_columns = compute_correlations(
+    # Calculate with Cramer's V is chosen
+
+    corr, sign, coef, columns, nominal_columns, dichotomous_columns, ordinal_columns, inf_nan, single_value_columns = compute_correlations(
         arbis_encoded,
         nominal_columns=nominal_columns, dichotomous_columns=dichotomous_columns, ordinal_columns=ordinal_columns,
         bias_correction=False)
@@ -100,10 +139,13 @@ if __name__ == '__main__':
                       single_value_columns, save=True, filepath=plot_path + 'arbis_dataset_corr_cramers.png',
                       show=True, figsize=(18, 15))
 
-    print(sign.to_latex())
+    test = sign.to_latex(float_format="{:0.2f}".format)
 
-    with open(plot_path + 'arbis_dataset_sign_cramers.tex', 'w') as tf:
+    with open(tex_path + 'arbis_dataset_sign_cramers.tex', 'w') as tf:
         tf.write(sign.to_latex(float_format="{:0.2f}".format))
+
+    with open(tex_path + 'arbis_dataset_coef_cramers.tex', 'w') as tf:
+        tf.write(coef.to_latex(float_format="{:0.2f}".format))
 
     # Calculate with Theil's U
     # associations(arbis_encoded, figsize=(18, 15),
@@ -113,18 +155,6 @@ if __name__ == '__main__':
     #     plt.savefig(plot_path + 'arbis_dataset_corr_theils.png')
     # plt.show()
 
-    plt.figure(figsize=(11, 6))
-    sns.set_context('paper', font_scale=1.0)
-    sns.boxplot(x='Strasse', y='Length', data=arbis_select_relevant, palette='Set1')
-    if safe_plots:
-        plt.savefig(plot_path + 'arbis_dataset_box_street2length.png')
-    plt.show()
 
-    plt.figure(figsize=(11, 6))
-    sns.set_context('paper', font_scale=1.0)
-    sns.boxplot(x='Strasse', y='Duration', data=arbis_select_relevant, palette='Set1')
-    if safe_plots:
-        plt.savefig(plot_path + 'arbis_dataset_box_stree2duration.png')
-    plt.show()
 
     print('Finished ArbIS Dataset Analysis')

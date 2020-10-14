4a4c3016557f65cd4f5f50e784e290f558847a5b
Jakob Erpf <contact@jakoberpf.de>
2020-10-14 09:46:11 +0200

    rename plot function and add statistic plot function

--- CorrAnalysis/func_plot.py
+++ CorrAnalysis/func_plot.py
@@ -22,23 +22,23 @@ import pandas as pd
 import seaborn as sns
 
 
-def plot_dataframe(corr, columns,
-                   nominal_columns=None,
-                   dichotomous_columns=None,
-                   ordinal_columns=None,
-                   inf_nan=None,
-                   single_value_columns=None,
-                   save=False,
-                   filepath='plot.png',
-                   show=True,
-                   ax=None,
-                   figsize=None,
-                   annot=True,
-                   fmt='.2f',
-                   cmap=None,
-                   sv_color='silver',
-                   cbar=True
-                   ):
+def plot_correlation(corr, columns,
+                     nominal_columns=None,
+                     dichotomous_columns=None,
+                     ordinal_columns=None,
+                     inf_nan=None,
+                     single_value_columns=None,
+                     save=False,
+                     filepath='plot.png',
+                     show=True,
+                     ax=None,
+                     figsize=None,
+                     annot=True,
+                     fmt='.2f',
+                     cmap=None,
+                     sv_color='silver',
+                     cbar=True
+                     ):
     """
 
     Parameters:
@@ -80,23 +80,25 @@ def plot_dataframe(corr, columns,
     """
 
     if ax is None:
+        # If no figure/mathplotlib is given, set new figure size
         plt.figure(figsize=figsize)
 
-    if inf_nan.any(axis=None):
-        inf_nan_mask = np.vectorize(lambda x: not bool(x))(inf_nan.values)
-        ax = sns.heatmap(inf_nan_mask,
-                         cmap=['white'],
-                         annot=inf_nan if annot else None,
-                         fmt='',
-                         center=0,
-                         square=True,
-                         ax=ax,
-                         mask=inf_nan_mask,
-                         cbar=False)
-    else:
-        inf_nan_mask = np.ones_like(corr)
+    # if inf_nan.any(axis=None):
+    #     inf_nan_mask = np.vectorize(lambda x: not bool(x))(inf_nan.values)
+    #     ax = sns.heatmap(inf_nan_mask,
+    #                      cmap=['white'],
+    #                      annot=inf_nan if annot else None,
+    #                      fmt='',
+    #                      center=0,
+    #                      square=True,
+    #                      ax=ax,
+    #                      mask=inf_nan_mask,
+    #                      cbar=False)
+    # else:
+    #     inf_nan_mask = np.ones_like(corr)
 
     if len(single_value_columns) > 0:
+        # If dataframe contains single value columns
         sv = pd.DataFrame(data=np.zeros_like(corr),
                           columns=columns,
                           index=columns)
@@ -124,7 +126,91 @@ def plot_dataframe(corr, columns,
                      fmt=fmt,
                      center=0,
                      vmax=1.0,
+                     # if there are only
                      vmin=-1.0 if len(columns) - len(nominal_columns) >= 2 else 0.0,
+                     square=True,
+                     mask=mask,
+                     ax=ax,
+                     cbar=cbar)
+
+    if save:
+        plt.savefig(filepath)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+    return ax
+
+
+def plot_statistic(corr, columns,
+                   nominal_columns=None,
+                   dichotomous_columns=None,
+                   ordinal_columns=None,
+                   inf_nan=None,
+                   single_value_columns=None,
+                   save=False,
+                   filepath='plot.png',
+                   show=True,
+                   ax=None,
+                   figsize=None,
+                   annot=True,
+                   fmt='.2f',
+                   cmap=None,
+                   sv_color='silver',
+                   cbar=True
+                   ):
+
+    if ax is None:
+        # If no figure/mathplotlib is given, set new figure size
+        plt.figure(figsize=figsize)
+
+    if inf_nan.any(axis=None):
+        inf_nan_mask = np.vectorize(lambda x: not bool(x))(inf_nan.values)
+        ax = sns.heatmap(inf_nan_mask,
+                         cmap=['white'],
+                         annot=inf_nan if annot else None,
+                         fmt='',
+                         center=0,
+                         square=True,
+                         ax=ax,
+                         mask=inf_nan_mask,
+                         cbar=False)
+    else:
+        inf_nan_mask = np.ones_like(corr)
+
+    if len(single_value_columns) > 0:
+        # If dataframe contains single value columns
+        sv = pd.DataFrame(data=np.zeros_like(corr),
+                          columns=columns,
+                          index=columns)
+        for c in single_value_columns:
+            sv.loc[:, c] = ' '
+            sv.loc[c, :] = ' '
+            sv.loc[c, c] = 'SV'
+        sv_mask = np.vectorize(lambda x: not bool(x))(sv.values)
+        ax = sns.heatmap(sv_mask,
+                         cmap=[sv_color],
+                         annot=sv if annot else None,
+                         fmt='',
+                         center=0,
+                         square=True,
+                         ax=ax,
+                         mask=sv_mask,
+                         cbar=False)
+    else:
+        sv_mask = np.ones_like(corr)
+
+    mask = np.vectorize(lambda x: not bool(x))(inf_nan_mask) + np.vectorize(lambda x: not bool(x))(sv_mask)
+    ax = sns.heatmap(corr,
+                     cmap=cmap,
+                     annot=annot,
+                     fmt=fmt,
+                     center=0,
+                     # vmax=1.0,
+                     # # if there are only
+                     # vmin=-1.0 if len(columns) - len(nominal_columns) >= 2 else 0.0,
                      square=True,
                      mask=mask,
                      ax=ax,

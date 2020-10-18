2a7784512a9112eaaa6caca095b5f3a76c2faaee
Jakob Erpf <contact@jakoberpf.de>
2020-10-18 10:22:04 +0200

    expand arbis matched

--- CorrAnalysis/main_arbis_02_matched.py
+++ CorrAnalysis/main_arbis_02_matched.py
@@ -17,6 +17,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from func_correlation import numerical_encoding, compute_correlations
+from func_plot import plot_boxplot_logscale, plot_correlation, plot_statistic
 from func_utils import date_parser, print_welcome
 
 if __name__ == '__main__':
@@ -29,6 +31,7 @@ if __name__ == '__main__':
     work_path = data_path + 'ArbIS/matched/'
     plot_path = work_path + 'plots/'
     tex_path = work_path + 'latex/'
+    csv_path = work_path + 'csv/'
     work_file = 'ArbIS_2019.csv'
 
     arbis_imported = pd.read_csv(work_path + work_file, sep=';', decimal=',', parse_dates=True, date_parser=date_parser)
@@ -37,12 +40,16 @@ if __name__ == '__main__':
         [
             # Congestion Data
             "TempExMax",
-            # "TempExMin [min]", # Not implemented
+            # "TempExMin", # Not implemented
             "SpatExMax",
-            # "SpatExMin [m]", # Not implemented
+            # "SpatExMin", # Not implemented
             "TempDist",
             "SpatDist",
             "Coverage",
+            "temporalGlobalLoc",
+            "spatialGlobalLoc",
+            "temporalInternalLoc",
+            "spatialInternalLoc",
             "TimeLossCar",
             "TimeLossHGV",
             # Accident Data
@@ -61,43 +68,156 @@ if __name__ == '__main__':
             "Length",
             "Duration"]].copy()
 
+    # Manual data type conversion from str to datetime64
+    arbis_imported['Von'] = pd.to_datetime(arbis_imported['Von'], format='%Y-%m-%d %H:%M:%S')
+    arbis_imported['Bis'] = pd.to_datetime(arbis_imported['Bis'], format='%Y-%m-%d %H:%M:%S')
+
+    # Manual data type conversion from str to int64
     arbis_selected["TimeLossCar"] = pd.to_numeric(arbis_selected["TimeLossCar"])
     arbis_selected["TimeLossHGV"] = pd.to_numeric(arbis_selected["TimeLossHGV"])
 
-    # Print matrix for debugging
-    print(arbis_selected.dtypes)
+    # Add month of roadwork
+    arbis_selected['Month'] = arbis_imported['Von'].dt.month_name()
 
-    # Plot association matrix
-    associations(arbis_selected, point_biserial=True, kruskal=False, theil_u=False, clustering=False,
-                 figsize=(18, 15),
-                 nominal_columns=["temporalGlobalLoc",
-                                  "spatialGlobalLoc",
-                                  "temporalInternalLoc",
-                                  "spatialInternalLoc",
-                                  "AnzGesperrtFs",
-                                  "Richtung",
-                                  "Strasse"
-                                  ],
-                 plot=False, bias_correction=False)
-    if safe_plots:
-        plt.savefig(plot_path + 'arbis_matched_corr_cramers.png')
-    plt.show()
+    # Plot histogram of roadworks over time / months
+    plt.figure(figsize=(13, 6))
+    plt.hist(arbis_selected['Month'], color='blue', edgecolor='black', label=True)
+    plt.title('Histogram of roadworks per month')
+    plt.ylabel('Count')
+    plt.xlabel('Month of 2019')
+    if save_plot:
+        plt.savefig(plot_path + 'arbis_dataset_hist_month.png')
+    if show_plot:
+        plt.show()
+    else:
+        plt.close()
 
-    # Plot association matrix
-    associations(arbis_selected, point_biserial=True, kruskal=False, theil_u=True, clustering=False,
-                 figsize=(18, 15),
-                 nominal_columns=["temporalGlobalLoc",
-                                  "spatialGlobalLoc",
-                                  "temporalInternalLoc",
-                                  "spatialInternalLoc",
-                                  "AnzGesperrtFs",
-                                  "Richtung",
-                                  "Strasse"
-                                  ],
-                 plot=False, bias_correction=False)
-    if safe_plots:
-        plt.savefig(plot_path + 'arbis_matched_corr_theils.png')
-    plt.show()
+    # Plot histogram of accidents over highway
+    plt.figure(figsize=(13, 6))
+    n, bins, patches = plt.hist(arbis_selected['Strasse'], color='blue', edgecolor='black', label=True)
+    plt.title('Histogram of roadworks per highways')
+    plt.ylabel('Count')
+    plt.xlabel('Highway')
+    if save_plot:
+        plt.savefig(plot_path + 'baysis_dataset_hist_highway.png')
+    if show_plot:
+        plt.show()
+    else:
+        plt.close()
+
+    # Plot boxplots for visual relation testing
+    plot_boxplot_logscale(arbis_selected, 'Strasse', 'Length', save_plot, show_plot,
+                          plot_path + 'arbis_dataset_box_street2length.png')
+
+    plot_boxplot_logscale(arbis_selected, 'Strasse', 'Duration', save_plot, show_plot,
+                          plot_path + 'arbis_dataset_box_street2duration.png')
+
+    plot_boxplot_logscale(arbis_selected, 'AnzGesperrtFs', 'Length', save_plot, show_plot,
+                          plot_path + 'arbis_dataset_box_agfs2length.png')
+
+    plot_boxplot_logscale(arbis_selected, 'AnzGesperrtFs', 'Duration', save_plot, show_plot,
+                          plot_path + 'arbis_dataset_box_agfs2duration.png')
+
+    plot_boxplot_logscale(arbis_selected, 'Einzug', 'Length', save_plot, show_plot,
+                          plot_path + 'arbis_dataset_box_einzug2length.png')
+
+    plot_boxplot_logscale(arbis_selected, 'Einzug', 'Duration', save_plot, show_plot,
+                          plot_path + 'arbis_dataset_box_einzug2duration.png')
+
+    plot_boxplot_logscale(arbis_selected, 'Richtung', 'Length', save_plot, show_plot,
+                          plot_path + 'arbis_dataset_box_direction2length.png')
+
+    plot_boxplot_logscale(arbis_selected, 'Richtung', 'Duration', save_plot, show_plot,
+                          plot_path + 'arbis_dataset_box_direction2duration.png')
+
+    # define column types
+    nominal_columns = ["temporalGlobalLoc",
+                       "spatialGlobalLoc",
+                       "temporalInternalLoc",
+                       "spatialInternalLoc",
+                       "AnzGesperrtFs",
+                       "Richtung",
+                       "Strasse", 'StreckeID', 'Month']
+    dichotomous_columns = ['Richtung']
+    ordinal_columns = ['AnzGesperrtFs', 'Einzug']
+
+    # Encode non numerical columns
+    arbis_encoded, arbis_encoded_dict = numerical_encoding(arbis_selected, nominal_columns, drop_single_label=False,
+                                                           drop_fact_dict=False)
+    arbis_encoded.to_csv(csv_path + 'encoded.csv', index=False, sep=';')
+
+    with open(csv_path + 'encoded_dict.csv', 'w') as tf:
+        for key in arbis_encoded_dict.keys():
+            tf.write("%s, %s\n" % (key, arbis_encoded_dict[key]))
+
+    # Calculate with Cramers 's V
+    results = None  # To make sure that no old data is reused
+    results = compute_correlations(
+        arbis_encoded,
+        columns_nominal=nominal_columns, columns_dichotomous=dichotomous_columns, columns_ordinal=ordinal_columns,
+        bias_correction=False)
+
+    # Plot correlation matrix
+    plot_correlation(results.get('correlation'), results.get('columns'),
+                     nominal_columns, dichotomous_columns, ordinal_columns,
+                     results.get('inf_nan_corr'),
+                     results.get('columns_single_value'),
+                     save=save_plot, filepath=plot_path + 'arbis_matched_corr_cramers.png',
+                     show=show_plot, figsize=(18, 15))
+
+    # Plot statistics/significant matrix
+    plot_statistic(results.get('significance'), results.get('columns'),
+                   nominal_columns, dichotomous_columns, ordinal_columns,
+                   results.get('inf_nan_corr'),
+                   results.get('columns_single_value'),
+                   save=save_plot, filepath=plot_path + 'arbis_matched_sign_cramers.png',
+                   show=show_plot, figsize=(18, 15))
+
+    # Export correlation/statistics/coefficients into latex tables
+    with open(tex_path + 'arbis_matched_corr_cramers.tex', 'w') as tf:
+        tf.write(results.get('correlation').to_latex(float_format="{:0.2f}".format))
+
+    with open(tex_path + 'arbis_matched_sign_cramers.tex', 'w') as tf:
+        tf.write(results.get('significance').to_latex())
+
+    with open(tex_path + 'arbis_matched_coef_cramers.tex', 'w') as tf:
+        tf.write(results.get('coefficient').to_latex())
+
+    # Calculate with Theil's U
+    results = None  # To make sure that no old data is reused
+    results = compute_correlations(
+        arbis_encoded,
+        theils=True,
+        columns_nominal=nominal_columns, columns_dichotomous=dichotomous_columns, columns_ordinal=ordinal_columns,
+        bias_correction=False)
+
+    # Plot correlation matrix
+    plot_correlation(results.get('correlation'), results.get('columns'),
+                     nominal_columns, dichotomous_columns, ordinal_columns,
+                     results.get('inf_nan_corr'),
+                     results.get('columns_single_value'),
+                     save=save_plot, filepath=plot_path + 'arbis_matched_corr_theils.png',
+                     show=show_plot, figsize=(18, 15))
+
+    # Plot statistics/significant matrix
+    plot_statistic(results.get('significance'), results.get('columns'),
+                   nominal_columns, dichotomous_columns, ordinal_columns,
+                   results.get('inf_nan_corr'),
+                   results.get('columns_single_value'),
+                   save=save_plot, filepath=plot_path + 'arbis_matched_sign_theils.png',
+                   show=show_plot, figsize=(18, 15))
+
+    # Export correlation/statistics/coefficients into latex tables
+    with open(tex_path + 'arbis_matched_corr_theils.tex', 'w') as tf:
+        tf.write(results.get('correlation').to_latex(float_format="{:0.2f}".format))
+
+    with open(tex_path + 'arbis_matched_sign_theils.tex', 'w') as tf:
+        tf.write(results.get('significance').to_latex())
+
+    with open(tex_path + 'arbis_matched_coef_theils.tex', 'w') as tf:
+        tf.write(results.get('coefficient').to_latex())
+
+    print('Finished ArbIS Dataset Analysis')
 
     # Plot scatter diagrams
     # Congestion -> Roadwork

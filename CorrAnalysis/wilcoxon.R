5e412adb6f59007741501028a515fe531e2eae29
Jakob Erpf <contact@jakoberpf.de>
2020-10-24 12:11:48 +0200

    update r script

--- CorrAnalysis/wilcoxon.R
+++ CorrAnalysis/wilcoxon.R
@@ -1,13 +1,22 @@
 args <- commandArgs(trailingOnly = TRUE)
 cat(args)
 
-library(readr)
 library(psych)
+library(readr)
 
-encoded <- read_csv("data/ArbIS/dataset/csv/encoded.csv")
+encoded <- read_delim("data/ArbIS/03_selected/csv/encoded.csv", 
+                      ";", escape_double = FALSE, trim_ws = TRUE)
+encoded <- read_delim("data/BAYSIS/03_selected/csv/encoded.csv", 
+                      ";", escape_double = FALSE, trim_ws = TRUE)
 
-kruskal.test(encoded$Length~encoded$Strasse)
+kruskal.test(encoded$Strasse~encoded$SpatExMax)
+kruskal.test(encoded$WoTag~encoded$SpatExMax)
+kruskal.test(encoded$Month~encoded$SpatExMax)
 
-pairwise.wilcox.test(encoded$Length,encoded$Strasse, paired = FALSE, p.adjust = "bonferroni")
 
-describeBy(encoded$Length,encoded$Strasse)
+# pairwise.wilcox.test(encoded$SpatExMax,encoded$Month, paired = FALSE, p.adjust = "bonferroni")
+pairwise.wilcox.test(encoded$SpatExMax,encoded$Month, paired = FALSE, p.adjust = "holm")
+pairwise.wilcox.test(encoded$SpatExMax,encoded$Strasse, paired = FALSE, p.adjust = "holm")
+
+describeBy(encoded$SpatExMax,encoded$Strasse)
+describeBy(encoded$SpatExMax,encoded$Month)

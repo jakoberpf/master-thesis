46dc117faf40b0a9c42134a2dc5a985ab0180028
Jakob Erpf <contact@jakoberpf.de>
2020-10-26 15:03:09 +0100

    update

--- CorrAnalysis/wilcoxon_baysis.R
+++ CorrAnalysis/wilcoxon_baysis.R
@@ -4,8 +4,6 @@ cat(args)
 library(psych)
 library(readr)
 
-encoded <- read_delim("data/ArbIS/03_selected/csv/encoded.csv", 
-                      ";", escape_double = FALSE, trim_ws = TRUE)
 encoded <- read_delim("data/BAYSIS/03_selected_01_duringAndInsideOfJam/csv/encoded.csv", 
                       ";", escape_double = FALSE, trim_ws = TRUE)
 
@@ -16,6 +14,7 @@ kruskal.test(encoded$WoTag~encoded$SpatExMax)
 kruskal.test(encoded$Month~encoded$SpatExMax)
 
 # pairwise.wilcox.test(encoded$SpatExMax,encoded$Month, paired = FALSE, p.adjust = "bonferroni")
+
 pairwise.wilcox.test(encoded$SpatExMax,encoded$Strasse, paired = FALSE, p.adjust = "holm")
 pairwise.wilcox.test(encoded$SpatExMax,encoded$UArt1, paired = FALSE, p.adjust = "holm")
 pairwise.wilcox.test(encoded$SpatExMax,encoded$AufHi, paired = FALSE, p.adjust = "holm")
@@ -23,4 +22,7 @@ pairwise.wilcox.test(encoded$SpatExMax,encoded$WoTag, paired = FALSE, p.adjust =
 pairwise.wilcox.test(encoded$SpatExMax,encoded$Month, paired = FALSE, p.adjust = "holm")
 
 describeBy(encoded$SpatExMax,encoded$Strasse)
+describeBy(encoded$SpatExMax,encoded$UArt1)
+describeBy(encoded$SpatExMax,encoded$AufHi)
+describeBy(encoded$SpatExMax,encoded$WoTag)
 describeBy(encoded$SpatExMax,encoded$Month)
